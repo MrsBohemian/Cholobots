15939a229c1c07bb07efd67bef9777f419036905
MrsBohemian <handley.heaven@gmail.com>
2026-04-30 15:40:20 -0500

    Update metichebot.py
    fixing indentation error

--- bots/metichebot.py
+++ bots/metichebot.py
@@ -570,15 +570,15 @@ Save quarterly and yearly goals
 """
         await ctx.send(msg)
 
-@bot.command(name="mschedule")
-async def mschedule(ctx: commands.Context):
-    active_daily_sessions.pop(ctx.channel.id, None)
-                
-    metiche = get_metiche()
-    
-    if metiche is None:
-        await ctx.send("Metiche isn’t initialized yet.")
-        return
+    @bot.command(name="mschedule")
+    async def mschedule(ctx: commands.Context):
+        active_daily_sessions.pop(ctx.channel.id, None)
+                    
+        metiche = get_metiche()
+        
+        if metiche is None:
+            await ctx.send("Metiche isn’t initialized yet.")
+            return
 
         def check(m: discord.Message):
             return (m.author.id == ctx.author.id) and (m.channel.id == ctx.channel.id)
